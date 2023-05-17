7793085c9c577a8ee531b4ccc78c8ab32b45977b
Mike Coon <mac718@gmail.com>
2023-05-17 10:17:06 -0700

    add query parameter to API call

--- src/controllers/spoonacular.mjs
+++ src/controllers/spoonacular.mjs
@@ -4,11 +4,11 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export const searchRecipes = async (req, res) => {
-  const { includeIngredients, excludeIngredients, diet } = req.body;
+  const { query, includeIngredients, excludeIngredients, diet } = req.body;
   const base_url = "https://api.spoonacular.com/recipes/complexSearch";
   try {
     const api_response = await axios.get(
-      `${base_url}?apiKey=${process.env.API_KEY}&includeIngredients=${includeIngredients}&excludeIngredients=${excludeIngredients}&diet=${diet}`
+      `${base_url}?apiKey=${process.env.API_KEY}&query=${query}&includeIngredients=${includeIngredients}&excludeIngredients=${excludeIngredients}&diet=${diet}`
     );
 
     res.json(api_response.data.results);
